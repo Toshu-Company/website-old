af087b6147254f25042be878071608986bf5d291
Anonymous <>
2023-08-26 20:18:54 +0900

    save store persistently

--- src/store/setting.ts
+++ src/store/setting.ts
@@ -1,9 +1,32 @@
+import { persistentMap } from "@nanostores/persistent";
 import { map } from "nanostores";
+
+export type RawSetting = {
+  censored: "true" | "false";
+};
 
 export interface Setting {
   censored: boolean;
 }
 
-export const $setting = map<Setting>({
-  censored: import.meta.env.PUBLIC_SCHOOL == "1",
+export const $rawSetting = persistentMap<RawSetting>("settings:", {
+  censored: import.meta.env.PUBLIC_SCHOOL == "1" ? "true" : "false",
 });
+
+export const $setting = map<Setting>(rawSettingToSetting($rawSetting.get()));
+
+$setting.listen((value) => {
+  $rawSetting.set(settingToRawSetting(value));
+});
+
+function rawSettingToSetting(raw: RawSetting): Setting {
+  return {
+    censored: raw.censored == "true" ? true : false,
+  };
+}
+
+function settingToRawSetting(setting: Setting): RawSetting {
+  return {
+    censored: setting.censored ? "true" : "false",
+  };
+}
