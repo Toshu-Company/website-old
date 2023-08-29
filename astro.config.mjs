f959b1a9d1fc335001699d9a48624908fa60bc8c
Anonymous <>
2023-08-29 09:07:12 +0900

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,27 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import react from "@astrojs/react";
 import compress from "astro-compress";
+import svelte from "@astrojs/svelte";
 const isDev = process.env.NODE_ENV === 'development';
+
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        assets: true
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: sharpImageService()
+  },
+  vite: {
+    legacy: {
+      buildSsrCjsExternalHeuristics: !isDev
     },
-    image: {
-        service: sharpImageService()
-    },
-    vite: {
-        legacy: {
-            buildSsrCjsExternalHeuristics: !isDev
-        },
-        ssr: {
-            noExternal: ['styled-components']
-        }
-    },
-    integrations: [react(), compress({
-        CSS: false,
-    })]
+    ssr: {
+      noExternal: ['styled-components']
+    }
+  },
+  integrations: [react(), svelte(), compress({
+    CSS: false
+  })]
 });
