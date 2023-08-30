46df441edbc8b96d0c40e08cd55f777d827daac1
Anonymous <>
2023-08-31 08:46:07 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
-import { defineConfig, sharpImageService } from 'astro/config';
+import { defineConfig, sharpImageService } from "astro/config";
 import react from "@astrojs/react";
 import compress from "astro-compress";
 import svelte from "@astrojs/svelte";
-const isDev = process.env.NODE_ENV === 'development';
-
+const isDev = process.env.NODE_ENV === "development";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true
+    assets: true,
   },
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
   },
   vite: {
     legacy: {
-      buildSsrCjsExternalHeuristics: !isDev
+      buildSsrCjsExternalHeuristics: !isDev,
     },
     ssr: {
-      noExternal: ['styled-components']
-    }
+      noExternal: ["styled-components"],
+    },
   },
-  integrations: [react(), svelte(), compress({
-    CSS: false
-  })]
+  integrations: [
+    react(),
+    svelte(),
+    compress({
+      CSS: false,
+    }),
+  ],
+  output: "hybrid",
 });
