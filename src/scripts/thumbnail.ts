de1be51c4173299d2d1e947a2292adcd68d83931
Anonymous <>
2023-09-12 00:19:23 +0900

    fix

--- src/scripts/thumbnail.ts
+++ src/scripts/thumbnail.ts
@@ -4,29 +4,6 @@ import { Blob } from "buffer";
 
 const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.2/dist/esm";
 
-const workers: {
-  ffmpeg: FFmpeg;
-  used: boolean;
-}[] = [];
-
-const generateWorker = async () => {
-  const ffmpeg = new FFmpeg();
-  await load(ffmpeg);
-  const worker = {
-    ffmpeg,
-    used: false,
-  };
-  workers.push(worker);
-  return worker;
-};
-
-export const getWorker = async () => {
-  const worker =
-    workers.find((worker) => !worker.used) ?? (await generateWorker());
-  worker.used = true;
-  return worker;
-};
-
 const _cache = new Map<string, string>();
 const cachedBlobURL = async (url: string, mimeType: string) => {
   if (_cache.has(url)) {
@@ -37,38 +14,111 @@ const cachedBlobURL = async (url: string, mimeType: string) => {
   return blobURL;
 };
 
-const cacheFile = async (key: string, blob: any) => {
+class Worker {
+  static workers: Worker[] = [];
+  static get count() {
+    return Worker.workers.length;
+  }
+  static get usedCount() {
+    return Worker.workers.filter((worker) => worker.used).length;
+  }
+
+  public used = false;
+  public ffmpeg: FFmpeg;
+  constructor() {
+    this.ffmpeg = new FFmpeg();
+    Worker.workers.push(this);
+  }
+
+  async load() {
+    console.log("loading ffmpeg");
+    await this.ffmpeg
+      .load({
+        coreURL: await cachedBlobURL(
+          `${baseURL}/ffmpeg-core.js`,
+          "text/javascript"
+        ),
+        wasmURL: await cachedBlobURL(
+          `${baseURL}/ffmpeg-core.wasm`,
+          "application/wasm"
+        ),
+      })
+      .catch(console.error);
+    console.log("ffmpeg loaded");
+  }
+
+  async close() {
+    this.used = false;
+  }
+
+  static async getWorker() {
+    const worker =
+      Worker.workers.find((worker) => !worker.used) ?? new Worker();
+    worker.used = true;
+    if (!worker.ffmpeg.loaded) {
+      await worker.load();
+    }
+    console.log("workers", Worker.count);
+    console.log("used", Worker.usedCount);
+    return worker;
+  }
+}
+
+const setFile = async (key: string, blob: any) => {
   const dataUrl = await blobToDataURL(blob);
   localStorage.setItem(key, dataUrl);
 };
 
-const cachedFile = async (key: string) => {
+const getFile = async (key: string) => {
   const dataUrl = localStorage.getItem(key);
   if (dataUrl) {
     return dataURLtoBlob(dataUrl);
   }
 };
 
-const load = async (ffmpeg: FFmpeg) => {
-  console.log("loading ffmpeg");
-  await ffmpeg
-    .load({
-      coreURL: await cachedBlobURL(
-        `${baseURL}/ffmpeg-core.js`,
-        "text/javascript"
-      ),
-      wasmURL: await cachedBlobURL(
-        `${baseURL}/ffmpeg-core.wasm`,
-        "application/wasm"
-      ),
-    })
-    .catch(console.error);
-  console.log("ffmpeg loaded");
-};
-
 export const translateVideoURL = (video: string) => {
   const url = new URL(video);
   return `${import.meta.env.PUBLIC_TWITTER_API_URL}/lover${url.pathname}`;
+};
+
+export const getThumbnail = async (video: string) => {
+  if (typeof window !== "undefined") {
+    const cached = await getFile(video);
+    if (cached) {
+      return URL.createObjectURL(cached);
+    }
+  }
+  const worker = await Worker.getWorker();
+  const ffmpeg = worker.ffmpeg;
+  await ffmpeg.writeFile("input.m3u8", await fetchFile(video));
+  await ffmpeg.writeFile(
+    "segmentNo0.js",
+    await fetchFile(video.split("/").slice(0, -1).join("/") + "/segmentNo0.js")
+  );
+  await ffmpeg.exec([
+    "-allowed_extensions",
+    "ALL",
+    "-i",
+    "input.m3u8",
+    "-ss",
+    "00:00:00.000",
+    "-vframes",
+    "1",
+    "-vf",
+    "scale=320:-1",
+    "output.jpg",
+  ]);
+  const data = await ffmpeg.readFile("output.jpg");
+  worker.close();
+  if (typeof data === "string") {
+    return data;
+  }
+  if (typeof window === "undefined") {
+    return data;
+  }
+  const blob: any = new Blob([data], { type: "image/jpeg" });
+  setFile(video, blob);
+  return URL.createObjectURL(blob);
 };
 
 //**dataURL to blob**
@@ -94,47 +144,3 @@ async function blobToDataURL(blob: any) {
     a.readAsDataURL(blob);
   });
 }
-
-export const getThumbnail = async (video: string) => {
-  if (typeof window !== "undefined") {
-    const cached = await cachedFile(video);
-    if (cached) {
-      return URL.createObjectURL(cached);
-    }
-  }
-  const worker = await getWorker();
-  const ffmpeg = worker.ffmpeg;
-  console.log("workers", workers.length);
-  if (!ffmpeg.loaded) {
-    await load(ffmpeg);
-  }
-  await ffmpeg.writeFile("input.m3u8", await fetchFile(video));
-  await ffmpeg.writeFile(
-    "segmentNo0.js",
-    await fetchFile(video.split("/").slice(0, -1).join("/") + "/segmentNo0.js")
-  );
-  await ffmpeg.exec([
-    "-allowed_extensions",
-    "ALL",
-    "-i",
-    "input.m3u8",
-    "-ss",
-    "00:00:00.000",
-    "-vframes",
-    "1",
-    "-vf",
-    "scale=320:-1",
-    "output.jpg",
-  ]);
-  const data = await ffmpeg.readFile("output.jpg");
-  worker.used = false;
-  if (typeof data === "string") {
-    return data;
-  }
-  if (typeof window === "undefined") {
-    return data;
-  }
-  const blob: any = new Blob([data], { type: "image/jpeg" });
-  cacheFile(video, await blobToDataURL(blob));
-  return URL.createObjectURL(blob);
-};
