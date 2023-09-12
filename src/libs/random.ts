95c7ba181dfd7f1a7cc4e11d8d1e6b71a0e7fe5e
Anonymous <>
2023-09-12 10:24:54 +0900

    remove crypto package

--- src/libs/random.ts
+++ src/libs/random.ts
@@ -1,16 +1,6 @@
-import crypto from "crypto";
-
 const used = new Set<string>();
 
 export function random(): string {
   const result = Math.random().toString(36).substring(2);
   return used.has(result) ? random() : result;
-}
-
-export function randomLength(length: number): string {
-  //   return crypto
-  //     .getRandomValues(new Uint8Array(length))
-  //     .reduce((p, i) => p + (i % 36).toString(36), "");
-  const result = crypto.randomBytes(length).toString("hex");
-  return used.has(result) ? randomLength(length) : result;
 }
