a16a2b24c274afd273a522e0188edde8eb8b2c3f
Anonymous <>
2023-12-18 07:03:09 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,9 +19,6 @@ export default defineConfig({
   integrations: [
     react(),
     svelte(),
-    compress({
-      CSS: false,
-    }),
   ],
   output: 'server',
   adapter: vercel({
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { styled } from "styled-components";
 
 type Responsive =
   | [
