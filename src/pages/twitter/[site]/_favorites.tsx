708777128d2f18a13fedd07dc635bec3f6378b8c
Anonymous <>
2025-01-01 15:04:33 +0900

    feat: data import & export

--- src/pages/twitter/[site]/_favorites.tsx
+++ src/pages/twitter/[site]/_favorites.tsx
@@ -14,19 +14,52 @@ export default function Index(props: Props) {
 
   const migrate = async () => {
     const old = new PersistentStore("twivideo");
-    const data = (await Promise.all(old.export().map((v) => provider.getVideo(v))));
+    const data = await Promise.all(
+      old.export().map((v) => provider.getVideo(v))
+    );
     console.log(data);
     if (provider.favorite.Type === "object") {
       console.log("object");
       await provider.favorite.import(data);
       console.log("imported");
     }
-  }
+  };
+
+  const _export = async () => {
+    const data = await provider.favorite.export();
+    const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `${props.provider}.json`;
+    a.click();
+  };
+
+  const _import = async (file: File) => {
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const data = JSON.parse(e.target!.result as string);
+      await provider.favorite.import(data);
+    };
+    reader.readAsText(file);
+  };
 
   return (
     <>
       <Wrapper>
-        <button onClick={() => migrate()}>Migrate</button>
+        <div>
+          <button onClick={() => migrate()}>Migrate</button>
+          <button onClick={() => _export()}>Export</button>
+          <button onClick={() => document.getElementById("file")?.click()}>
+            Import
+          </button>
+          <input
+            id="file"
+            type="file"
+            style={{ display: "none" }}
+            onChange={(e) => _import(e.target.files![0])}
+          />
+        </div>
         <Content.Container>
           {favorites.map((v, i) => (
             <Content.Item
